a61953fb0d40acf9c2bc6bd4359ca19d9d6f17fa
Ingrid Lohmann <ingrid.lohmann@db1.com.br>
2021-09-10 17:33:27 -0300

    Ajustes componente SplitScreen

--- pokedex/src/components/SplitScreen/index.js
+++ pokedex/src/components/SplitScreen/index.js
@@ -1,50 +1,20 @@
 import React from 'react';
-import { Input } from 'antd';
-
-import logo from "../../assets/pokedex-logo.png";
-
-// import squirtle from "../../assets/squirtle.png";
 
 import styles from "./styles.module.scss";
 
-const SplitScreen = ({ children }) => {
-
-    const { Search } = Input;
+const SplitScreen = ({ leftSide, rightSide }) => {
 
     return (
         <div className={styles.container}>
 
             <div className={styles.leftSide}>
 
-                <div className={styles.squirtle}>
-                    <img
-                        src={require('../../assets/squirtle.png').default}
-                        alt="squirtle"
-                        width={323}
-                    />
-                </div>
-
-                <img src={logo} className={styles.pokedexLogo} alt="logo" />
-
-                <Search
-                    placeholder="Digite aqui o pokémon"
-                    enterButton
-                    size="large"
-                    className={styles.input}
-                />
-
-                <div className={styles.pikachu}>
-                    <img
-                        src={require('../../assets/pikachu.png').default}
-                        alt="pikachu"
-                        width={323}
-                    />
-                </div>
+                {leftSide}
 
             </div>
 
             <div className={styles.rightSide}>
-                {children}
+                {rightSide}
             </div>
 
         </div>
